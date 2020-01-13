062deb64ef55c1a716b0351615726009b6e1e687
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-13 22:08:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,9 @@ def monopoly_with_fourth_tier
        :four_pieces_owned => 200
      },
      :names => {
-       :reading_railroad => {$100},
+       :reading_railroad => {
+         ""
+       },
        :pennsylvania_railroad => {},
        :b_and_o_railroad => {},
        :shortline_railroad => {}
